2bea2917dc0f2adadbdafd85f95559c92f994feb
Martin <martin.erlic@gmail.com>
2025-11-04 23:37:59 +0100

    adjust starting items, tree graphics

--- server/src/starting_items.rs
+++ server/src/starting_items.rs
@@ -26,22 +26,22 @@ pub(crate) fn grant_starting_items(ctx: &ReducerContext, player_id: Identity, us
         // Hotbar (Slots 0-5)
         ("Combat Ladle", 1, Some(0u8), None), 
         ("Torch", 10, Some(1u8), None),
-        ("Furnace", 1, Some(2u8), None),
-        ("Camp Fire", 1, Some(4u8), None),
-        ("Repair Hammer", 1, Some(5u8), None),
+        // ("Furnace", 1, Some(2u8), None),
+        // ("Camp Fire", 1, Some(4u8), None),
+        // ("Repair Hammer", 1, Some(5u8), None),
 
         // Starting materials in Inventory (Slots 0-23)
-        ("Sleeping Bag", 1, None, Some(0u16)),
-        ("Wooden Storage Box", 1, None, Some(1u16)),
-        ("Wood", 1000, None, Some(2u16)),
-        ("Stone", 1000, None, Some(3u16)),
-        ("Metal Ore", 1000, None, Some(4u16)),
-        ("Raw Twigfish", 20, None, Some(5u16)),
-        ("Hunting Bow", 1, None, Some(6u16)),
-        ("Wooden Arrow", 50, None, Some(7u16)),
-        ("Reed Water Bottle", 1, None, Some(8u16)),
-        ("Shelter", 1, None, Some(9u16)),
-        ("Memory Shard", 5000, None, Some(10u16)),
+        // ("Sleeping Bag", 1, None, Some(0u16)),
+        // ("Wooden Storage Box", 1, None, Some(1u16)),
+        // ("Wood", 1000, None, Some(2u16)),
+        // ("Stone", 1000, None, Some(3u16)),
+        // ("Metal Ore", 1000, None, Some(4u16)),
+        // ("Raw Twigfish", 20, None, Some(5u16)),
+        // ("Hunting Bow", 1, None, Some(6u16)),
+        // ("Wooden Arrow", 50, None, Some(7u16)),
+        // ("Reed Water Bottle", 1, None, Some(8u16)),
+        // ("Shelter", 1, None, Some(9u16)),
+        // ("Memory Shard", 5000, None, Some(10u16)),
     ];
 
     log::info!("[GrantItems] Defined {} starting inventory/hotbar item entries.", starting_inv_items.len());
